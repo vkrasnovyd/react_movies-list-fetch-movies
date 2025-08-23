c3b0d7eaabc3852164f5e633b4d06243eedcc13b
vkrasnovyd <vkrasnovyd@proton.me>
2025-08-23 23:55:38 +0200

    Update fetching link

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,7 @@ import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
-  const apiKey = import.meta.env.VITE_OMDb_API_KEY;
-  const API_URL = `https://www.omdbapi.com/?apikey=${apiKey}`;
+  const API_URL = `https://www.omdbapi.com/?apikey=a2e75ac3`;
 
   return fetch(`${API_URL}&t=${query}`)
     .then(res => res.json())
