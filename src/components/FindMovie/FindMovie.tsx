37b931e601d9e09fa1b6bd232b79989109de7066
vkrasnovyd <vkrasnovyd@proton.me>
2025-08-23 23:53:13 +0200

    Set defaults for the movie

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -11,9 +11,12 @@ import { Movie } from '../../types/Movie';
 function getMovieFromData(data: MovieData): Movie {
   return {
     title: data.Title,
-    description: data.Plot,
-    imgUrl: data.Poster,
-    imdbUrl: `https://www.imdb.com/de/title/${data.imdbID}/`,
+    description: data.Plot || '',
+    imgUrl:
+      data.Poster !== 'N/A'
+        ? data.Poster
+        : 'https://via.placeholder.com/360x270.png?text=no%20preview',
+    imdbUrl: `https://www.imdb.com/title/${data.imdbID}`,
     imdbId: data.imdbID,
   };
 }
